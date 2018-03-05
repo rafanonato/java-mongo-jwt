9d89c9fc1a8d6ecf80d56e7465661fabb1b9369d
rafanonato <rf.nonato@gmail.com>
2018-03-05 17:58:37 -0300

    Config do mongo

--- src/main/java/br/com/crudmongo/nosql/Document/Usuario.java
+++ src/main/java/br/com/crudmongo/nosql/Document/Usuario.java
@@ -4,7 +4,7 @@ package br.com.crudmongo.nosql.Document;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "usuarios")
+@Document
 public class Usuario {
 
     private String id;
